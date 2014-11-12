5f13444f832e39696b04f1bf029609f69032b5d3
Patrick Herrmann <patrickwherrmann@gmail.com>
2014-11-11 16:50:22 -0800

    Assume every county has pop and area data

--- public/js/populationMap.js
+++ public/js/populationMap.js
@@ -1,7 +1,4 @@
 function coloration(county, pop, area) {
-    if (!pop || !area) {
-        console.log(county);
-    }
     var shade = (pop / area) / 40000;
     shade = Math.pow(shade, 0.25);
     return one.color('#e00')
